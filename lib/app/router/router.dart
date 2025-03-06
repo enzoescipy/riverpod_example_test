1b89459cc7ef65e8800d3b71398fd32e041267f5
enzoescipy <enzoescipy@gmail.com>
2025-03-06 20:08:48 +0900

    Revert "learning about shellRoute"
    This reverts commit b2aa7092d847e864686c8019b72e793ba8091f77.

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -1,28 +1,7 @@
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-import 'package:image_search/app/screen/Root/root_page.dart';
+import 'package:image_search/app/screen/testpage/testpage_page.dart';
 
-abstract class RouterOutlet {
-  // static final RouterOutlet _instance = RouterOutlet._internal();
-
-  // factory RouterOutlet() {
-  //   return _instance;
-  // }
-
-  // RouterOutlet._internal();
-
-  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
-  static final _shellNavigatorKey = GlobalKey<NavigatorState>();
-
-  static final router = GoRouter(
-    // initialLocation: Route.root,
-    navigatorKey: _rootNavigatorKey,
-    routes: [
-      // GoRoute(path: Route.root, builder: (context, state) => const RootPage(), routes: [ShellRoute(buil)]),
-      ShellRoute(routes: routes)
-    ],
-  );
-}
+final router = GoRouter(routes: <GoRoute>[GoRoute(path: Route.root, builder: (context, state) => const TestPage())]);
 
 abstract class Route {
   static const root = _Path.root;
